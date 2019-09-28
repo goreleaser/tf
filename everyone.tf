e02ae9c68e6ed5146517dc3963c839048903fa9f
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-09-28 12:02:59 -0300

    feat: everyone
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

---
+++ everyone.tf
@@ -0,0 +1,51 @@
+resource "github_team" "everyone" {
+  name        = "everyone"
+  description = "Everyone working on GoReleaser!"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "everyone_caarlos0" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.caarlos0.login
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "everyone_client9" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.client9.login
+}
+
+resource "github_team_membership" "everyone_crazy" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.crazy-max.login
+}
+
+resource "github_team_membership" "everyone_elopio" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.elopio.login
+}
+
+resource "github_team_membership" "everyone_jorinvo" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.jorinvo.login
+}
+
+resource "github_team_membership" "everyone_jspc" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.jspc.login
+}
+
+resource "github_team_membership" "everyone_lalloni" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.lalloni.login
+}
+
+resource "github_team_membership" "everyone_mavogel" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.mavogel.login
+}
+
+resource "github_team_membership" "everyone_mgrachev" {
+  team_id  = github_team.everyone.id
+  username = data.github_user.mgrachev.login
+}
---
+++ users.tf
@@ -0,0 +1,35 @@
+data "github_user" "caarlos0" {
+  username = "caarlos0"
+}
+
+data "github_user" "client9" {
+  username = "client9"
+}
+
+data "github_user" "crazy-max" {
+  username = "crazy-max"
+}
+
+data "github_user" "elopio" {
+  username = "elopio"
+}
+
+data "github_user" "jorinvo" {
+  username = "jorinvo"
+}
+
+data "github_user" "jspc" {
+  username = "jspc"
+}
+
+data "github_user" "lalloni" {
+  username = "lalloni"
+}
+
+data "github_user" "mavogel" {
+  username = "mavogel"
+}
+
+data "github_user" "mgrachev" {
+  username = "mgrachev"
+}

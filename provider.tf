bb76837c204a8fe80229622c8dfbf268df6b6569
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-09-28 12:13:46 -0300

    fix: provider
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- provider.tf
+++ provider.tf
@@ -2,3 +2,16 @@ provider "github" {
   token        = "${var.github_token}"
   organization = "goreleaser"
 }
+
+terraform {
+  required_version = "~> 0.12.0"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "goreleaser"
+
+    workspaces {
+      name = "tf"
+    }
+  }
+}

4d88e6a281174319c7355fb5d3ca43cd7210c743
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-10-01 14:31:01 -0300

    fix: improve tf protection
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- tf.tf
+++ tf.tf
@@ -1,4 +1,17 @@
 resource "github_repository" "tf" {
-  name        = "tf"
-  description = "Terraform resources for GoReleaser's organization"
+  name               = "tf"
+  description        = "Terraform resources for GoReleaser's organization"
+  allow_merge_commit = false
+  allow_rebase_merge = false
+}
+
+resource "github_branch_protection" "tf" {
+  repository     = "${github_repository.tf.name}"
+  branch         = "master"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = false
+    contexts = ["atlas/goreleaser/tf"]
+  }
 }
